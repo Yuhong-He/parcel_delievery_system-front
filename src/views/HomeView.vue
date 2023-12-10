ac60670d01610dd6491b8b9834fe94ce3525c4a0
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-12-10 14:50:18 +0000

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1>This is the frontend of a college project Parcel Delivery System, just beginning...</h1>
-  <h1>This commit is use to test Jenkins.</h1>
+  <div>
+    <h1>This is the frontend of a college project Parcel Delivery System, just beginning...</h1>
+    <h1>This commit is use to test Jenkins.</h1>
+  </div>
 </template>
 
 <script>
