3a6816564e02a9c16939eb78042e189e91bf8a6e
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-12-12 12:52:57 +0000

    test jenkins frankfurt server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>This is the frontend of a college project Parcel Delivery System, just beginning...</h1>
     <h1>This commit is use to test Jenkins.</h1>
+    <h1>This commit is use to test Jenkins in Frankfurt server.</h1>
   </div>
 </template>
 
