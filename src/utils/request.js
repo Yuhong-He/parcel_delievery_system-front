0800f7349f7309ee3a441ef2d1bbe12521c03dfc
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2024-01-05 03:40:17 +0000

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,8 +5,8 @@ import router from "@/router";
 import store from "@/store";
 import {cleanUserInfo} from "@/utils/user";
 
-const Base_URL = 'http://localhost:18080'; // dev, front: dev.ucdparcel.ie
-// const Base_URL = 'https://api.ucdparcel.ie'; // prod, front: www.ucdparcel.ie
+// const Base_URL = 'http://localhost:18080'; // dev, front: dev.ucdparcel.ie
+const Base_URL = 'https://api.ucdparcel.ie'; // prod, front: www.ucdparcel.ie
 
 const service = axios.create({
     timeout: 30000,
